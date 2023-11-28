7ff3fb5df44f7d9c990c982bcef4b78fb1152b1a
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-11-28 05:41:31 +0100

    add category breakdown section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import BudgetChart from '@/components/budget-chart';
-import { NairaIcon } from '@/components/icons';
+import { FoodnDrinkIcon, NairaIcon, SavingsIcon } from '@/components/icons';
 import {
   Box,
   Container,
@@ -12,50 +12,96 @@ import {
 
 export default function Home() {
   return (
-    <Container minH="100vh" bg="bdg.neutral.50">
-      <Box as="header">
-        <Heading pt="20px">Budget</Heading>
-        <HStack mt="28px" gap="10px">
-          <NairaIcon boxSize="20px" />
-          <Text>Monthly Budget</Text>
-        </HStack>
-        <Flex
-          w="full"
-          h="51px"
-          bg="bdg.white"
-          borderRadius="10px"
-          mt="10px"
-          pl="20px"
-          align="center"
-          boxShadow="0px 5px 10px 2px rgba(0, 0, 0, 0.03)"
-        >
-          <Heading>₦120,000</Heading>
-        </Flex>
-      </Box>
-      <Flex align="center" justify="center" mt="30px" pos="relative">
-        <BudgetChart />
-        <Text
-          pos="absolute"
-          left="50%"
-          transform="translate(-50%)"
-          fontSize="4xl"
-          fontWeight="bold"
-          color="bdg.primary.100"
-        >
-          49%
-        </Text>
-      </Flex>
-      <VStack gap="5px">
-        <Text textAlign="center" mt="21px">
-          Amount spent so far
-        </Text>
-        <Text fontSize="16px" color="bdg.primary.100">
-          ₦50,000
-          <Text as="span" color="bdg.primary.50">
-            /₦120,000
+    <Box minH="100vh" bg="bdg.neutral.50">
+      <Container>
+        <Box as="header">
+          <Heading pt="20px">Budget</Heading>
+          <HStack mt="28px" gap="10px">
+            <NairaIcon boxSize="20px" />
+            <Text>Monthly Budget</Text>
+          </HStack>
+          <Flex
+            w="full"
+            h="51px"
+            bg="bdg.white"
+            borderRadius="10px"
+            mt="10px"
+            pl="20px"
+            align="center"
+            boxShadow="0px 5px 10px 2px rgba(0, 0, 0, 0.03)"
+          >
+            <Heading>₦120,000</Heading>
+          </Flex>
+        </Box>
+        <Flex align="center" justify="center" mt="30px" pos="relative">
+          <BudgetChart />
+          <Text
+            pos="absolute"
+            left="50%"
+            transform="translate(-50%)"
+            fontSize="4xl"
+            fontWeight="bold"
+            color="bdg.primary.100"
+          >
+            49%
           </Text>
-        </Text>
+        </Flex>
+        <VStack gap="5px" pb="33px">
+          <Text textAlign="center" mt="21px">
+            Amount spent so far
+          </Text>
+          <Text fontSize="16px" color="bdg.primary.100">
+            ₦50,000
+            <Text as="span" color="bdg.primary.50">
+              /₦120,000
+            </Text>
+          </Text>
+        </VStack>
+
+        <Heading variant="h3">Category Breakdown</Heading>
+      </Container>
+
+      {/* Breakdown Section */}
+      <VStack
+        bg="#FCFEFF"
+        borderRadius="10px"
+        mt="10px"
+        pt="20px"
+        px="30px"
+        align="flex-start"
+        gap="20px"
+      >
+        <HStack justifyContent="space-between" w="full">
+          <HStack gap="10px">
+            <FoodnDrinkIcon boxSize="50px" />
+            <VStack align="flex-start" justify="center" gap="0">
+              <Heading variant="h2">Food and Drink</Heading>
+              <Text>40%</Text>
+            </VStack>
+          </HStack>
+          <Heading variant="sub-heading" flexShrink="0">
+            ₦20,000/
+            <Heading as="span" fontSize="inherit" color="bdg.neutral.100">
+              ₦42,000
+            </Heading>
+          </Heading>
+        </HStack>
+        <HStack justifyContent="space-between" w="full">
+          <HStack gap="10px">
+            <SavingsIcon boxSize="50px" />
+            <VStack align="flex-start" justify="center" gap="0">
+              <Heading variant="h2">Savings</Heading>
+              <Text>20%</Text>
+            </VStack>
+          </HStack>
+          <Heading variant="sub-heading" flexShrink="0">
+            ₦10,000/
+            <Heading as="span" fontSize="inherit" color="bdg.neutral.100">
+              ₦24,000
+            </Heading>
+          </Heading>
+        </HStack>
       </VStack>
-    </Container>
+    </Box>
   );
 }
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as NairaIcon } from './naira-icon';
 export { default as DoughnutChart } from './doughnut-chart';
+export { default as FoodnDrinkIcon } from './foodndrink-icon';
+export { default as SavingsIcon } from './savings-icon';
